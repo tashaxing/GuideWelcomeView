6b4530201a8f108c251c486601067efbe78f481b
Ethan He <tashaxing123@163.com>
2016-10-11 10:47:45 +0800

    add

---
+++ GuideWelcomeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuideWelcomeView
+//
+//  Created by yxhe on 16/10/11.
+//  Copyright © 2016年 tashaxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuideWelcomeView/MainViewController.m
@@ -0,0 +1,29 @@
+//
+//  MainViewController.m
+//  GuideWelcomeView
+//
+//  Created by yxhe on 16/10/11.
+//  Copyright © 2016年 tashaxing. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
